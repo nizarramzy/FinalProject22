c3c7c366059f8b27ca0d12605e3c3e509ac4bac9
usernameGitHubKamu <emailGitHubKamu>
2025-10-02 14:02:02 +0700

    FinalProjectRamzy

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("halo ini baris kode pertama");
+        System.out.println("halo saya Danendra Dewani No.8, melakukan edit code");
     }
 }
